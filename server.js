9c3d3cf42bf63684d7c515ccd0528eff57fc2f4d
pfilatov92 <pavel.filatov@gmail.com>
2017-03-10 13:31:39 +0700

    .htaccess add

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+var path = require('path');
 var express = require('express');
 var http = require('http');
 var bodyParser = require('body-parser');
@@ -8,9 +9,17 @@ var app = express();
 var sessions = require('express-session');
 var MongoStore = require('connect-mongo')(sessions);
 var Cart = require('./models/cart');
+var RewriteMiddleware = require('express-htaccess-middleware');
+var RewriteOptions = {
+  file: path.resolve(__dirname, '.htaccess'),
+  verbose: (process.env.ENV_NODE == 'development'),
+  watch: (process.env.ENV_NODE == 'development'),
+  server: app,
+};
 
 app.set('port', process.env.PORT || config.get('port'))
 
+app.use(RewriteMiddleware(RewriteOptions));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use(cookieParser());
@@ -22,6 +31,7 @@ app.use(sessions({
   cookie: config.get("session:cockie"),
   store: new MongoStore({mongooseConnection: mongoose.connection})
 }));
+
 app.use('/', require('./controllers'));
 
 http.createServer(app).listen(app.get('port'), function() {
